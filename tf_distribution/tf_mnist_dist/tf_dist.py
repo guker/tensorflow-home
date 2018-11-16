ef27f062ce666382ebffb903d581ea269fc52699
qwhu <qwhu@qwhudeMacBook-Pro.local>
2018-11-16 13:13:45 +0800

    null

--- tf_distribution/tf_mnist_dist/tf_dist.py
+++ tf_distribution/tf_mnist_dist/tf_dist.py
@@ -14,12 +14,17 @@ IMAGE_PIXELS = 28
 
 flags.DEFINE_string('data_dir', './mnist-data', 'Directory for storing mnist data')
 flags.DEFINE_integer('hidden_units', 100, 'Number of units in the hidden layer of the NN')
-flags.DEFINE_integer('train_steps', 15000, 'Number of training steps to perform')
+flags.DEFINE_integer('train_steps', 300000, 'Number of training steps to perform')
 flags.DEFINE_integer('batch_size', 128, 'Training batch size')
 flags.DEFINE_float('learning_rate', 0.01, 'Learning rate')
+flags.DEFINE_boolean('use_gpu', False, 'use gpu to accelate computing ')
+flags.DEFINE_integer('gpu_id', 0, 'multi-gpu server')
+flags.DEFINE_string('checkpoint', './checkpoint', 'checkpoint path')
+flags.DEFINE_string('logdir', './log', 'Training log')
 
 flags.DEFINE_string('ps_hosts', '192.168.32.73:2260', 'comma-seprated list of hostname:port pairs')
-flags.DEFINE_string('worker_hosts', '192.168.32.73:2261,192.168.32.73:2262', 'comma-separated list of hostname:port pairs')
+flags.DEFINE_string('worker_hosts', '192.168.32.73:2261',
+                    'comma-separated list of hostname:port pairs')
 
 flags.DEFINE_string('job_name', None, 'job name: worker or ps')
 flags.DEFINE_integer('task_index', None, 'Index of task within the job')
@@ -28,10 +33,37 @@ flags.DEFINE_boolean('sync_replicas', False, 'Use the sync_replicas (synchronize
 FLAGS = flags.FLAGS
 
 
+def set_learning_rate(learning_rate):
+    '''
+    tf.train.piecewise_constant 分段常数衰减
+    tf.train.polynomial_decay   多项式衰减
+    tf.train.exponential_decay  指数衰减
+    tf.train.natural_exp_decay  自然指数衰减
+    tf.train.cosine_decay       余弦衰减
+    tf.train.linear_cosine_decay 线性余弦衰减
+    ...
+    '''
+    pass
+
+
+def set_optimizer(optimizer, learning_rate):
+    if optimizer == 'SGD':
+        return tf.train.GradientDescentOptimizer(learning_rate)
+    elif optimizer == 'Adadelta':
+        return tf.train.AdadeltaOptimizer(learning_rate)
+    elif optimizer == 'Adagrad':
+        return tf.train.AdagradOptimizer(learning_rate)
+    elif optimizer == 'Adam':
+        return tf.train.AdamOptimizer(learning_rate)
+    elif optimizer == 'Momentum':
+        return tf.train.MomentumOptimizer(learning_rate)
+    elif optimizer == 'RMSProp':
+        return tf.train.RMSProp(learning_rate)
+
 
 def create_done_queue(num_worker):
     with tf.device("/job:ps/task:0"):
-        return tf.FIFOQueue(num_worker,tf.int32,shared_name='done_queue')
+        return tf.FIFOQueue(num_worker, tf.int32, shared_name='done_queue')
 
 
 def main(unused_argv):
@@ -56,17 +88,19 @@ def main(unused_argv):
 
     server = tf.train.Server(cluster, job_name=FLAGS.job_name, task_index=FLAGS.task_index)
     if FLAGS.job_name == 'ps':
-        #server.join()
+        # server.join()
 
         with tf.Session(server.target) as sess:
             for i in range(num_worker):
                 sess.run(kill_ps_queue.dequeue())
         return
 
-
     is_chief = (FLAGS.task_index == 0)
+    if FLAGS.use_gpu:
+        worker_device = '/job:worker/task:%d/gpu:%d' % (FLAGS.task_index, FLAGS.gpu_id)
+    else:
+        worker_device = '/job:worker/task:%d/cpu:0' % FLAGS.task_index
 
-    worker_device = '/job:worker/task:%d/cpu:0' % FLAGS.task_index
     with tf.device(tf.train.replica_device_setter(
             worker_device=worker_device,
             ps_device='/job:ps/cpu:0',
@@ -89,14 +123,16 @@ def main(unused_argv):
 
         y = tf.nn.softmax(tf.nn.xw_plus_b(hid, sm_w, sm_b))
         cross_entropy = -tf.reduce_sum(y_ * tf.log(tf.clip_by_value(y, 1e-10, 1.0)))
+        tf.summary.scalar('loss', cross_entropy)
 
-        opt = tf.train.AdamOptimizer(FLAGS.learning_rate)
+        # opt = tf.train.AdamOptimizer(FLAGS.learning_rate)
+        opt = set_optimizer('Adam', FLAGS.learning_rate)
 
         if FLAGS.sync_replicas:
             replicas_to_aggregate = num_worker
             opt = tf.train.SyncReplicasOptimizer(
                 opt,
-                replicas_to_aggregate = replicas_to_aggregate,
+                replicas_to_aggregate=replicas_to_aggregate,
                 total_num_replicas=num_worker,
                 use_locking=False,
                 name='mnist_sync_replicas')
@@ -112,26 +148,31 @@ def main(unused_argv):
             chief_queue_runner = opt.get_chief_queue_runner()
             init_token_op = opt.get_init_tokens_op(0)
 
-
         init_op = tf.global_variables_initializer()
         kill_ps_enqueue_op = kill_ps_queue.enqueue(1)
 
-        train_dir = tempfile.mkdtemp()
+        summary_op = tf.summary.merge_all()
+        summary_writer = tf.summary.FileWriter(FLAGS.logdir)
+        saver = tf.train.Saver()
+
+        # train_dir = tempfile.mkdtemp()
 
         if FLAGS.sync_replicas:
-            sv = tf.train.Supervisor(is_chief=is_chief, logdir=train_dir,
-                                 init_op=init_op, local_init_op=local_init_op,
-                                 ready_for_local_init_op=ready_for_local_init_op,
-                                 recovery_wait_secs=1, global_step=global_step)
+            sv = tf.train.Supervisor(is_chief=is_chief, logdir=FLAGS.checkpoint,
+                                     init_op=init_op, local_init_op=local_init_op,
+                                     ready_for_local_init_op=ready_for_local_init_op,
+                                     summary_op=summary_op, saver=saver, summary_writer=summary_writer,
+                                     recovery_wait_secs=1, global_step=global_step)
         else:
-            sv = tf.train.Supervisor(is_chief=is_chief,logdir=train_dir,
-                                     init_op=init_op,recovery_wait_secs=1,
+            sv = tf.train.Supervisor(is_chief=is_chief, logdir=FLAGS.checkpoint,
+                                     init_op=init_op, recovery_wait_secs=1,
+                                     summary_op=summary_op, saver=saver, summary_writer=summary_writer,
                                      global_step=global_step)
 
         sess_config = tf.ConfigProto(
             allow_soft_placement=True,
             log_device_placement=False,
-            device_filters=['/job:ps','/job:worker/task:%d'%FLAGS.task_index]
+            device_filters=['/job:ps', '/job:worker/task:%d' % FLAGS.task_index]
         )
 
         if is_chief:
@@ -144,8 +185,7 @@ def main(unused_argv):
 
         if FLAGS.sync_replicas and is_chief:
             sess.run(init_token_op)
-            sv.start_queue_runners(sess,[chief_queue_runner])
-            print('init_token_op finish')
+            sv.start_queue_runners(sess, [chief_queue_runner])
 
         time_begin = time.time()
 
@@ -157,9 +197,9 @@ def main(unused_argv):
             batch_xs, batch_ys = mnist.train.next_batch(FLAGS.batch_size)
             train_feed = {x: batch_xs, y_: batch_ys}
 
-
-            _, step = sess.run([train_step, global_step], feed_dict=train_feed)
-            print('*'*50)
+            _, summary_res, step = sess.run([train_step, summary_op, global_step], feed_dict=train_feed)
+            sv.summary_writer.add_summary(summary_res, step)
+            print('*' * 50)
 
             local_step += 1
 
